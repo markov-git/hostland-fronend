4d09143ba1a3d1c3f6874232ac3705534494905d
markov <markovartyom795@gmail.com>
2022-01-03 23:02:16 +0300

    Added tailwind to project

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 export function Home(): JSX.Element {
 	return (
-		<h1>Hello world</h1>
+		<h1
+			className={'text-3xl font-bold underline'}
+		>Hello world</h1>
 	);
 }
